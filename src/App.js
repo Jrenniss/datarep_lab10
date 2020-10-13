682719d40c50eee3cea97f05906dbb60579be23c
Jrenniss <jrenniss@gmail.com>
2020-10-14 00:43:27 +0100

    Lab 6(a) Routing for Navbar

--- src/App.js
+++ src/App.js
@@ -5,26 +5,35 @@ import { Header } from './components/header';
 import { Footer } from './components/footer';
 import { Content } from './components/content';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 //Change from function to class
 class App extends Component {
-  render(){
-  return (
-    //Components - Contain content to make one web page.
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/read">Read</Nav.Link>
-            <Nav.Link href="/create">Create</Nav.Link>
-          </Nav>
-        </Navbar>
-            <Header></Header>
-            <Content></Content>
-            <Footer></Footer>
-    </div>
-  );
+  render() {
+    return (
+      <Router>
+            //Components - Contain content to make one web page.
+        <div className="App">
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+
+          <br />
+          <Switch>
+            <Route path='/' component={Content} exact />
+            <Route path='/create' component={Header} exact />
+            <Route path='/read' component={Footer} exact />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
