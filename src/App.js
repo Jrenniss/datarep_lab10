c068b556206d46e66682a47361009e9e00b920ed
Jrenniss <jrenniss@gmail.com>
2020-10-13 23:42:19 +0100

    Lab 3(b,c) Class, Time Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <h1>Hello World</h1>
+  <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
+  }
 }
 
 export default App;
