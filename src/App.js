dd6a740994e7f4e884d504fad1e4bdb9b29b129d
Jrenniss <jrenniss@gmail.com>
2020-10-14 00:35:18 +0100

    Lab 5(a,b) Bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import './App.css';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
 import { Content } from './components/content';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from 'react-bootstrap';
 //Change from function to class
 class App extends Component {
   render(){
   return (
     //Components - Contain content to make one web page.
     <div className="App">
-      <Header></Header>
-      <Content></Content>
-      <Footer></Footer>
+      <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Navbar>
+            <Header></Header>
+            <Content></Content>
+            <Footer></Footer>
     </div>
   );
   }
