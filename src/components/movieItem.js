b67c54a05003079e9f857c2ca29ae9e65c10a8ce
Jrenniss <jrenniss@gmail.com>
2020-10-21 13:58:07 +0100

    Movie Item Component Added

---
+++ src/components/movieItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//MovieItem Component
+export class MovieItem extends React.Component{
+    //Method to display what is within
+    render(){
+        return(
+             //Contents on display in the Component
+            <div>
+                <h1>This is the Movie Item Component.</h1>
+            </div>
+        );
+    }
+}
